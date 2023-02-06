88e82c07bcdf66809dcc9efb8432991acb9ea201
kave haj <kave.haj@snapp.cab>
2023-02-06 21:07:02 +0330

    added echo and run server

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"echo-server/config"
+	"fmt"
+	"log"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
+	//confi
+	err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(config.Cfg.Port)
+
+	//init server
+	server := echo.New()
+
+	//start server
+	err = server.Start(":"+config.Cfg.Port)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
