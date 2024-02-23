410fe9da2a62ff794182241a9dfe3a4032ff6e57
kave08 <kave.mailbox@gmail.com>
2024-02-23 23:13:26 +0330

    init mongodb

--- database/db.go
+++ database/db.go
@@ -1,1 +1,20 @@
 package database
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func InitMongo() {
+	ctx := context.TODO()
+
+	client, err := mongo.Connect(
+		ctx,
+	)
+	if err != nil {
+		return
+	}
+	fmt.Println(client)
+}
