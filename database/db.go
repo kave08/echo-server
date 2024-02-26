af0859c142f28c08d132d1b39b1dc330c6d45545
kave08 <kave.mailbox@gmail.com>
2024-02-26 17:29:59 +0330

    added mongodb

--- database/db.go
+++ database/db.go
@@ -2,19 +2,41 @@ package database
 
 import (
 	"context"
-	"fmt"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func InitMongo() {
+type Db struct {
+	client *mongo.Client
+}
+
+func InitMongo() (*Db, error) {
 	ctx := context.TODO()
 
 	client, err := mongo.Connect(
 		ctx,
+		options.Client().ApplyURI("mongodb://localhost:27017"),
 	)
 	if err != nil {
-		return
+		return nil, err
 	}
-	fmt.Println(client)
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+
+	return &Db{
+		client: client,
+	}, nil
+
+}
+
+func (db Db) GetUserCollection() *mongo.Collection {
+
+	userCollection := db.client.Database("mydb").Collection("users")
+
+	return userCollection
 }
