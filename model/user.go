8fd9950606d2c3fe30c49eb6daf0fbdf54735ea5
kave08 <kave.mailbox@gmail.com>
2024-02-26 17:30:30 +0330

    added id based on mongodb typ

--- model/user.go
+++ model/user.go
@@ -1,6 +1,9 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type User struct {
+	Id     primitive.ObjectID
 	Name   string `query:"name" json:"first_name"`
 	Family string `query:"family" json:"last_name"`
 	Age    int    `query:"age" json:"age"`
