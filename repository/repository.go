54106513dc2fd4b37dec36659b5ba9baf6340957
kave08 <kave.mailbox@gmail.com>
2024-02-26 20:05:34 +0330

    added getuserid

--- repository/repository.go
+++ repository/repository.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"echo-server/database"
 	"echo-server/model"
+	"log"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Repository interface {
@@ -17,7 +19,13 @@ type repository struct {
 }
 
 func NewUserService() Repository {
-	return repository{}
+	db, err := database.InitMongo()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return repository{
+		db: *db,
+	}
 }
 
 func (r repository) GetUserList() ([]model.User, error) {
@@ -27,12 +35,38 @@ func (r repository) GetUserList() ([]model.User, error) {
 
 	cur, err := c.Find(context.TODO(), bson.D{})
 	if err != nil {
+		
 		return nil, err
 	}
 	err = cur.All(context.TODO(), &users)
 	if err != nil {
+
 		return nil, err
 	}
 
 	return users, nil
+}
+
+func (r repository) GetUserId(id string) (*model.User, error) {
+	var user model.User
+
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+
+		return nil, err
+	}
+
+	c := r.db.GetUserCollection()
+
+	err = c.FindOne(context.TODO(), bson.D{
+		{
+			Key: "_id", Value: objectId,
+		},
+	}).Decode(&user)
+	if err != nil {
+
+		return nil, err
+	}
+
+	return &user, nil
 }
