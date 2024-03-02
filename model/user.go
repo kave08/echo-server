ecd2668f3415bd15876661d621a90604d1567750
kave08 <kave.mailbox@gmail.com>
2024-03-02 21:22:33 +0330

    handle omitempty

--- model/user.go
+++ model/user.go
@@ -1,9 +1,9 @@
 package model
 
 type User struct {
-	Id     string `bson:"_id"`
-	Name   string `bson:"_id" query:"name" json:"first_name"`
-	Family string `bson:"_id" query:"family" json:"last_name"`
-	Age    int    `bson:"_id" query:"age" json:"age"`
-	Phone  string `bson:"_id" query:"phone" json:"phone"`
+	Id     string `bson:"_id,omitempty"`
+	Name   string `bson:"name,omitempty" query:"name" json:"first_name"`
+	Family string `bson:"family,omitempty" query:"family" json:"last_name"`
+	Age    int    `bson:"age,omitempty" query:"age" json:"age"`
+	Phone  string `bson:"phone,omitempty" query:"phone" json:"phone"`
 }
