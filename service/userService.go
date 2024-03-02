d694d71c76aaa480d732ddda59b46a51b8b58828
kave08 <kave.mailbox@gmail.com>
2024-03-02 21:23:38 +0330

    added insert user

--- service/userService.go
+++ service/userService.go
@@ -17,7 +17,21 @@ func (u userService) GetUserList() ([]model.User, error) {
 	userRepo := repository.NewUserService()
 	userList, err := userRepo.GetUserList()
 	if err != nil {
+
 		return nil, err
 	}
+
+	_, err = userRepo.InsertUser(model.User{
+		Name:   "",
+		Family: "",
+		Age:    0,
+		Phone:  "",
+	})
+
+	if err != nil {
+
+		return nil, err
+	}
+
 	return userList, nil
 }
