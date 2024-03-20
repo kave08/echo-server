57577bb74ff7694bafb3d11b790bd053c410c99c
kave08 <kave.mailbox@gmail.com>
2024-03-21 00:00:23 +0330

    added get user interface

--- service/interface.go
+++ service/interface.go
@@ -5,4 +5,5 @@ import "echo-server/model"
 type UserService interface {
 	CreateUser(user model.User) (string, error)
 	GetUserList() ([]model.User, error)
+	GetUserByUserNameAndPassword(login model.Login) (*model.User, error)
 }
