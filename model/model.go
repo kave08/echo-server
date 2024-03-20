cfbf80d87a7954e0c9ee13046ba6fd2c167a6b47
kave08 <kave.mailbox@gmail.com>
2024-03-20 12:39:55 +0330

    added login moddel

--- model/model.go
+++ model/model.go
@@ -1,1 +1,6 @@
 package model
+
+type Login struct {
+	UserName string `validate:"required"  query:"user_name" json:"user_name"`
+	Password string `validate:"required"  query:"password" json:"password"`
+}
