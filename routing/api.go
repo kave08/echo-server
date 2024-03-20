acfdec310815fcec8d2848422346b6f09e995619
kave08 <kave.mailbox@gmail.com>
2024-03-20 13:07:18 +0330

    added middleware

--- routing/api.go
+++ routing/api.go
@@ -1,14 +1,20 @@
 package routing
 
 import (
+	"echo-server/common/security"
 	"echo-server/handler"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func SetRouting(e *echo.Echo, handler *handler.UserHandler) error {
 
-	e.POST("/login", handler.Login())
+	jwtCfg := middleware.JWTConfig{
+		SigningKey: []byte("secret "),
+		Claims:     security.JtwClaims{},
+	}
+	e.POST("/login", handler.Login(), middleware.JWTWithConfig(jwtCfg))
 
 	u := e.Group("/users")
 	u.GET("/getlist", handler.GetUserList())
