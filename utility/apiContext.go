68a7bee1c5be2400e5520005b01d3b16b07933ad
kave08 <kave.mailbox@gmail.com>
2024-05-01 13:39:47 +0330

    recover panic

--- utility/apiContext.go
+++ utility/apiContext.go
@@ -2,16 +2,29 @@ package utility
 
 import (
 	"echo-server/common/security"
+	"errors"
 
 	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 )
 
-type ApiContext struct {
+// APIContext is a custom context that embeds the echo.Context and provides additional functionality.
+type APIContext struct {
 	echo.Context
 }
 
-func (a ApiContext) GetUserId() (string, error) {
+// GetUserID retrieves the user ID from the JWT token stored in the context.
+//
+// It returns an empty string and an error if the user is not logged in.
+func (a APIContext) GetUserID() (userID string, err error) {
+	defer func() {
+		r := recover()
+		if r != nil {
+			userID = ""
+			err = errors.New("user is not login")
+
+		}
+	}()
 	token := a.Get("user").(*jwt.Token)
 
 	claim := token.Claims.(*security.JtwClaims)
